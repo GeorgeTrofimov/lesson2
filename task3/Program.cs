131c3d4821fa7bb0aa281bf5b7aa40e8c9aa2029
George <tro37295@gmail.com>
2022-07-01 23:30:03 +0500

    task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information1
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Prompt(string message){
+    Console.Write (message);
+    int a = int.Parse (Console.ReadLine());
+    return a;
+}
+
+int n = Prompt("Введите трехзначное число N >");
+while (Math.Abs(n) > 1000)
+{
+n = n / 10;
+}
+if (n>99){
+   System.Console.WriteLine(n%10); 
+}
+else System.Console.WriteLine("Третьей цифры нет");
