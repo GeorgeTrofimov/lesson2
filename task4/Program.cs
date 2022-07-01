cfa9c97fb0af327fd8ab7c33ff410331b28f3b56
George <tro37295@gmail.com>
2022-07-01 23:39:14 +0500

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+int Prompt(string message){
+    Console.Write (message);
+    int a = int.Parse (Console.ReadLine());
+    return a;
+}
+
+int day = Prompt("Введите цифру обоздачающий день недели ");
+if (day< 1 || day> 7)
+{
+    Console.WriteLine("Введены некорретные числа");
+}
+else if (day == 6 || day == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Рабочий день");
+}
