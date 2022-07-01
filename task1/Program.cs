7b8ec6ebf92ad2d2a5dbfa91c051758971c7346b
George <tro37295@gmail.com>
2022-07-01 22:58:30 +0500

    task2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string message){
+    Console.Write (message);
+    int a = int.Parse (Console.ReadLine());
+    return a;
+}
+
+int n = Prompt("Введите трехзначное число N >");
+if (n>99 && n<1000){
+   System.Console.WriteLine((n%100)/10); 
+}
+else System.Console.WriteLine("Вы ввели не трехзначное число");
